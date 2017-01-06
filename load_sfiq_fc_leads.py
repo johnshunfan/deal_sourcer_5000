56cd4becf1e48d00c123769562a3d2e92d42cda8
John Fan <jsf.fan@gmail.com>
2017-01-06 11:44:46 -0800

    Initial commit

---
+++ load_sfiq_fc_leads.py
@@ -0,0 +1,131 @@
+#! /usr/bin/python
+
+# load all of salesforce IQ fc leads into the database
+# TODO load id as well to edit?
+import csv
+import ConfigParser
+import traceback
+import re
+import requests
+import json
+import re
+from time import time
+from sqlalchemy import Column, BigInteger, Integer, Float, DateTime, String, Index
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sp_util import format_string, format_number, format_date
+
+def printJ(jData):
+    print json.dumps(jData, indent=4, sort_keys=True)
+
+def getUserName(userId, API_KEY, API_SECRET):
+    getUserUrl = 'https://api.salesforceiq.com/v2/users/{userId}'
+    getUserUrl = getUserUrl.replace('{userId}', userId)
+    response = requests.get(getUserUrl, auth=(API_KEY, API_SECRET))
+    data = json.loads(response.text)
+    printJ(data)
+    return json.loads(response.text)['name']
+
+def select_list_items(list_id, start, limit, API_KEY, API_SECRET):
+    insertUrl = 'https://api.salesforceiq.com/v2/lists/{0}/listitems?_start={1}&_limit={2}'.format(list_id, start, limit)
+    response = requests.get(insertUrl, auth=(API_KEY, API_SECRET))
+    data = json.loads(response.text)
+    #printJ(data)
+    return data
+
+Base = declarative_base()
+
+class FcLead(Base):
+    __tablename__ = 'fc_leads'
+    #tell SQLAlchemy the name of column and its attributes:
+    id = Column(Integer, primary_key=True, nullable=False)
+    name = Column(String(255))
+    domain = Column(String(255))
+    fc_lead = Column(String(255))
+    __table_args__ = (Index('name', 'name'), Index('fcl', 'fc_lead'))
+
+def get_fc_leads(fc_lead_dict, companies, session):
+    s = session
+    print 'looking up and loading'
+    #companies = get_all_list_items(NEWCO_LIST_ID, API_KEY, API_SECRET)
+    #companies = select_list_items(NEWCO_LIST_ID, 2000, 200, API_KEY, API_SECRET)
+    for i in range(len(companies['objects'])):
+        fc_lead_id = companies['objects'][i]['fieldValues']['1'][0]['raw']
+        fc_lead_str = ''
+        if not fc_lead_id in fc_lead_dict:
+            print 'looking up ' + str(fc_lead_id)
+            try:
+                fc_lead_dict[fc_lead_id] = getUserName(fc_lead_id, API_KEY, API_SECRET)
+            except:
+                fc_lead_dict[fc_lead_id] = fc_lead_id
+            print 'looking up ' + str(fc_lead_id) + ': ' + fc_lead_dict[fc_lead_id]
+        fc_lead_str = fc_lead_dict[fc_lead_id]
+        try:
+            domain = companies['objects'][i]['fieldValues']['87'][0]['raw']
+        except:
+            domain = ''
+        try:
+            record = FcLead(**{
+                'name':companies['objects'][i]['name'],
+                'domain':domain,
+                'fc_lead':fc_lead_str,
+            })
+            s.add(record) #Add all the records
+        except:
+            print 'error in: ' + str(i) + ', ' + companies['objects'][i]['name'] + ':' + traceback.format_exc()
+        if (i % 1000 == 0 or i == len(companies['objects']) - 1):
+            print 'index: ' + str(i)
+            # if can't commit, then rollback
+            try:
+                s.commit()
+            except:
+                s.rollback() #Rollback the changes on error
+                print 'Unexpected error on index ' + str(i) + ':' + traceback.format_exc()
+                break
+
+    print 'loaded data'
+    return fc_lead_dict
+
+def get_all_list_items(list_id, API_KEY, API_SECRET, limit = 0):
+    # foundation salesforce
+    API_KEY= '581312c7e4b04c9692fadf3e'
+    API_SECRET= '1383QhosG3Eh4JUDoWLTRa0RnFr'
+    # FC lead is field id '1'
+    NEWCO_LIST_ID = '56fae761e4b07e602ad2e0fe'
+
+    t = time()
+
+    #Create the database
+    print 'connecting to mysql database'
+    config = ConfigParser.ConfigParser()
+    config.read('properties.ini')
+    engine = create_engine(config.get('properties', 'engine_string'))
+    Base.metadata.create_all(engine)
+
+    #Create the session
+    session = sessionmaker()
+    session.configure(bind=engine)
+    s = session()
+
+    size = select_list_items(list_id, 0, 0, API_KEY, API_SECRET)['totalSize'] if limit == 0 else limit
+    print 'size=' + str(size)
+    index = 0
+    fc_lead_dict = {}
+    while index <= size:
+        data = select_list_items(list_id, index, 200, API_KEY, API_SECRET)
+        fc_lead_dict = get_fc_leads(fc_lead_dict, data, session=s)
+        print 'finished loading index: ' + str(index) + ' to: ' + str(index+200)
+        index += 200
+    print 'completed all iterations of loading data'
+
+
+    #userId = (item for item in data['objects'] if item['name'].lower() == companyName.lower()).next()['fieldValues']['1'][0]['raw']
+
+if __name__ == "__main__":
+    API_KEY= '581312c7e4b04c9692fadf3e'
+    API_SECRET= '1383QhosG3Eh4JUDoWLTRa0RnFr'
+    NEWCO_LIST_ID = '56fae761e4b07e602ad2e0fe'
+    get_all_list_items(NEWCO_LIST_ID, API_KEY, API_SECRET)
+    # TODO why does putting in 2050 work but not 0? auto loading size doesn't work??
+
---
+++ main.py
@@ -0,0 +1,76 @@
+from flask import Flask, request
+from time import time
+import os
+import MySQLdb
+import webapp2
+
+from load_sfiq_fc_leads import get_all_list_items
+
+# Note: We don't need to call run() since our application is embedded within
+# the App Engine WSGI application server.
+
+# These environment variables are configured in app.yaml.
+CLOUDSQL_CONNECTION_NAME = os.environ.get('CLOUDSQL_CONNECTION_NAME')
+CLOUDSQL_USER = os.environ.get('CLOUDSQL_USER')
+CLOUDSQL_PASSWORD = os.environ.get('CLOUDSQL_PASSWORD')
+
+app = Flask(__name__)
+app.config['DEBUG'] = True
+
+def connect_to_cloudsql():
+    # When deployed to App Engine, the `SERVER_SOFTWARE` environment variable
+    # will be set to 'Google App Engine/version'.
+    if os.getenv('SERVER_SOFTWARE', '').startswith('Google App Engine/'):
+        # Connect using the unix socket located at
+        # /cloudsql/cloudsql-connection-name.
+        cloudsql_unix_socket = os.path.join(
+            '/cloudsql', CLOUDSQL_CONNECTION_NAME)
+
+        db = MySQLdb.connect(
+            unix_socket=cloudsql_unix_socket,
+            user=CLOUDSQL_USER,
+            passwd=CLOUDSQL_PASSWORD,
+            db='test3')
+
+    # If the unix socket is unavailable, then try to connect using TCP. This
+    # will work if you're running a local MySQL server or using the Cloud SQL
+    # proxy, for example:
+    #
+    #   $ cloud_sql_proxy -instances=your-connection-name=tcp:3306
+    #
+    else:
+        db = MySQLdb.connect(
+            host='127.0.0.1', user=CLOUDSQL_USER, passwd=CLOUDSQL_PASSWORD)
+
+    return db
+
+
+
+@app.route('/comment/<row_id>', methods=['GET', 'POST'])
+def hello(row_id):
+    db = connect_to_cloudsql()
+    comment = request.form['comment'][:1000]
+
+    cursor = db.cursor()
+    cmd = 'UPDATE companies SET comment="{0}" WHERE id={1}'.format(comment, row_id)
+    cursor.execute(cmd)
+    resp = ''
+    for r in cursor.fetchall():
+        resp = resp + str(','.join(r))
+
+    db.commit()
+    db.close()
+
+    return cmd
+
+@app.route('/refresh_newco', methods=['GET'])
+def refresh_newco():
+    API_KEY= '581312c7e4b04c9692fadf3e'
+    API_SECRET= '1383QhosG3Eh4JUDoWLTRa0RnFr'
+    NEWCO_LIST_ID = '56fae761e4b07e602ad2e0fe'
+    get_all_list_items(NEWCO_LIST_ID, API_KEY, API_SECRET)
+
+@app.errorhandler(404)
+def page_not_found(e):
+    """Return a custom 404 error."""
+    return 'Sorry, nothing at this URL.', 404
